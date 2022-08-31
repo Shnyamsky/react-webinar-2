ceb90492d12a9c6b841b224b336ec01b6d408e9d
Shnyamsky <ivanov.ilya-iv@yandex.ru>
2022-09-01 01:43:00 +0500

    1st commit

--- src/config.js
+++ src/config.js
@@ -1,20 +1,20 @@
 /**
  * Настройки сервисов
  */
-const config ={
+const config = {
   store: {
     log: false,
 
     modules: {
       session: {
-        tokenHeader: 'X-Token'
-      }
-    }
+        tokenHeader: 'X-Token',
+      },
+    },
   },
 
   api: {
-    baseUrl: ''
-  }
-}
+    baseUrl: '',
+  },
+};
 
 export default config;
